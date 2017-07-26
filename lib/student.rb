e0fd385f7979997b97bdc819fbc3017c8f0425f7
lglayat <glayatle@g.cofc.edu>
2017-07-26 15:23:40 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,70 @@
 require_relative "../config/environment.rb"
-
+require 'pry'
 class Student
 
+
+  attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def initialize(id=nil, name, grade)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql= <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def self.create(name, grade)
+    stud = Student.new(name, grade)
+    stud.save
+    stud
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql= <<-SQL
+        INSERT INTO students(name, grade) VALUES (? , ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    student = new_from_db(result)
+    student
+
+  end
+
+  def self.new_from_db(tuple)
+    student = self.new(tuple[0],tuple[1],tuple[2])
+    student
+  end
+
 
 
 end
